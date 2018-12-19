a3f150c3732ff8d9a5477cd52cff074d8e5bcf29
kumasys <kumagai@kumasys.com>
2018-12-19 16:27:39 +0900

    resolving issues

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import imaplib
 import email
 import base64
 import quopri
-import codecs
+
+from typing import Dict
 
 
 def get_body_text(payload):
@@ -43,31 +44,61 @@ def get_body_text(payload):
 
     return _type, body
 
+
+def get_credencial(credential):
+
+    _id, _secret = ""
+
+    if "basic" in credential.keys():
+        try:
+            _id = credential["basic"]["email_address"]
+            _secret = credential["basic"]["password"]
+        except Exception as e:
+            print(e)
+
+    elif "oauth2" in credential.keys():
+        try:
+            _id = credential["oauth2"]["email_address"]
+            _secret = credential["oauth2"]["access_token"]
+        except Exception as e:
+            print(e)
+
+    return _id, _secret
+
+
 search_term = "SINCE 9-Dec-2018 SEEN"
 
 
-with imaplib.IMAP4_SSL(host="imap.gmail.com", port=993) as mail:
-    mail.login("yoshinao.kumagai@gmail.com", "jhgyt2006!")
-    mail.select(mailbox='INBOX', readonly=False)
-    typ, mails = mail.search(None, search_term)
-    #typ, data = mail.fetch(b'80100', '(RFC822)') #80100
+def get_messages(service_name="", credential: Dict):
 
-    for _mail in mails[0].split():
-        _typ, data = mail.fetch(_mail, '(RFC822)')
+    get_credencial(credential)
 
-        raw_mail = email.message_from_bytes(data[0][1])
+    with imaplib.IMAP4_SSL(host="imap.gmail.com", port=993) as mail:
 
-        # get email encoding
-        email_encoding = email.header.decode_header(raw_mail.get('Subject'))[0][1] or 'iso-2022-jp'
+        if method is "basic" or method is not None:
 
-        # check for multipart
-        is_multipart = raw_mail.is_multipart()
+        mail.authenticate('XOAUTH2', lambda x: auth_string)
 
-        if is_multipart:
-            for payload in raw_mail.get_payload():
-                _type, _body = get_body_text(payload)
+        mail.select(mailbox='INBOX', readonly=False)
 
-        else:
-            _type, _body = get_body_text(raw_mail)
+        typ, mails = mail.search(None, search_term)
 
-        print(_body)
+        for _mail in mails[0].split():
+            _typ, data = mail.fetch(_mail, '(RFC822)')
+
+            raw_mail = email.message_from_bytes(data[0][1])
+
+            # get email encoding
+            email_encoding = email.header.decode_header(raw_mail.get('Subject'))[0][1] or 'iso-2022-jp'
+
+            # check for multipart
+            is_multipart = raw_mail.is_multipart()
+
+            if is_multipart:
+                for payload in raw_mail.get_payload():
+                    _type, _body = get_body_text(payload)
+
+            else:
+                _type, _body = get_body_text(raw_mail)
+
+            print(_body)
